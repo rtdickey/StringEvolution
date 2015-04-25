f89835ba22ba7090cfd536f7ca90097da4a8f10b
Daniel Nguyen <danielhuannguyen@gmail.com>
2015-04-24 21:20:38 -0500

    Added Comment

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,5 +7,6 @@ public class Main {
         System.out.println("Hello World");
         System.out.println("Hello Moon");
         System.out.println("This is the working copy");
+        //comment
     }
 }
