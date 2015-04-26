48414c7fac4b142becef303bd5ce37097802a64f
Daniel Nguyen <danielhuannguyen@gmail.com>
2015-04-26 16:10:11 -0500

    Made a small change to main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,7 +12,6 @@ public class Main {
         try
         {
             final String os = System.getProperty("os.name");
-
             if (os.contains("Windows"))
             {
                 Runtime.getRuntime().exec("cls");
